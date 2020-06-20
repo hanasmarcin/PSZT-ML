bb4a3e8f255cf0376250a72c27c3d667f7b35eea
hanasmarcin <m.hanas.dev@gmail.com>
2020-06-20 16:58:22 +0200

    Neural network finished, data retrieved.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,3 +1,6 @@
+# PSZT-ML Neural Network project
+# Hanas Marcin, Tuzimek Radosław
+# PW
 import numpy as np
 
 
@@ -8,26 +11,8 @@ def activation_fun(data):
     :param lmbd: parameter of activation function steepness
     :return: vector with activation function values for each neuron in one layer
     """
-
-    result = [2/(1 + np.exp2(-0.2*value)) - 1 for value in data]
-    # result = [(2/np.pi) * np.arctan(value) for value in data]
+    result = [2/np.pi*np.arctan(value) for value in data]
     return np.asarray(result)
-
-
-def activation_fun_lin(data):
-    # result = [max(min(value, 4), 0) for value in data]
-    # return np.asarray(result)
-    return data
-    # result = [np.arctan(10*value)/np.pi + np.arctan(10*(value-1))/np.pi + np.arctan(10*(value-2))/np.pi + np.arctan(10*(value-3))/np.pi + 2 for value in data]
-    # return np.asarray(result)
-
-
-def activation_fun_lin_dv(data):
-    # result = [1 if 0 <= value <= 4 else 0 for value in data]
-    # return np.asarray(result)
-    return np.ones(data.shape)
-    # result = [(1/np.pi)/(1 + (10*value)**2) + (1/np.pi)/(1 + (10*(value-1))**2) + (1/np.pi)/(1 + (10*(value-2))**2) + (1/np.pi)/(1 + (10*(value-3))**2) for value in data]
-    # return np.asarray(result)
 
 
 def activation_fun_dv(data):
@@ -36,20 +21,22 @@ def activation_fun_dv(data):
     :param data: ector with weighted sum of inputs for each neuron in one layer
     :return: vector with activation function derivative values for each neuron in one layer
     """
-    # result = [(2/np.pi)/(1 + value**2) for value in data]
-    # return np.asarray(result)
-    data_act_val = activation_fun(data)
-    return 0.5 * (np.ones(data_act_val.shape) - np.power(data_act_val, 2))
+    result = [2/np.pi/(1 + value**2) for value in data]
+    return np.asarray(result)
 
 
 class NeuralNetwork:
+    """
+    Class implements neural network
+    """
 
-    def __init__(self, input_count, network_shape):
+    def __init__(self, input_count, network_shape, learning_rate):
         """
         Constructor for the NeuralNetwork class
         :param input_count: number of inputs of the neural network
         :param network_shape: vector containing number of neurons for each layer
         """
+        self.learning_rate = learning_rate
         self.input_count = input_count
         self.network_shape = network_shape
 
@@ -61,12 +48,13 @@ class NeuralNetwork:
         self.layers_act_dv = []
 
         # Create first layer of network - first array of random coefficients
-        first_layer = np.random.random([network_shape[0], input_count + 1]) * 1
+        np.random.seed(10)
+        first_layer = (np.random.rand(network_shape[0], input_count + 1) - 0.5*np.ones([network_shape[0], input_count + 1])) / np.sqrt(input_count + 1)
         self.neuron_layers.append(first_layer)
 
         # Create rest of the layers with random coefficients
         for layer_count in range(1, network_shape.shape[0]):
-            layer = np.random.random([network_shape[layer_count], network_shape[layer_count - 1] + 1]) * 1
+            layer = (np.random.rand(network_shape[layer_count], network_shape[layer_count - 1] + 1) - 0.5*np.ones([network_shape[layer_count], network_shape[layer_count - 1] + 1])) / np.sqrt(network_shape[layer_count - 1] + 1)
             self.neuron_layers.append(layer)
             # print(layer_count)
         # print(self.neuron_layers)
@@ -92,8 +80,6 @@ class NeuralNetwork:
             else:
                 x = activation_fun(net)
                 derivative = activation_fun_dv(net)
-                # x = net
-                # derivative = np.ones(net.shape)
 
             # Add calculated outputs of one layer to the list
             self.layers_output.append(x)
@@ -102,13 +88,21 @@ class NeuralNetwork:
         return self.layers_output[-1]
 
     def propagate(self, nn_input, desired_output):
+        """
+        Method changes network's weights using backpropagation method for given element
+        :param nn_input: vector with input values for backpropagation
+        :param desired_output: vector with desired output values for given input
+        :return: value of total error for given element
+        """
         layers_dq_ds = []
         layers_dq_dth = []
 
         self.run(nn_input)
 
+        # For each layer, starting with output layer
         for layer_id in range(len(self.neuron_layers) - 1, -1, -1):
             if layer_id == len(self.neuron_layers) - 1:
+                # If this is an output layer
                 dq_dy = 2*(self.layers_output[layer_id] - desired_output)
             else:
                 dq_dy = layers_dq_ds[0] @ (self.neuron_layers[layer_id + 1][:, 0:-1])
@@ -117,51 +111,15 @@ class NeuralNetwork:
             if layer_id > 0:
                 dq_dth = np.outer(dq_ds, np.append(self.layers_output[layer_id - 1], 1))
             else:
+                # If this is the first layer
                 dq_dth = np.outer(dq_ds, np.append(nn_input, 1))
 
+            # Add calculated values to lists
             layers_dq_ds.insert(0, dq_ds)
             layers_dq_dth.insert(0, dq_dth)
 
+        # Change weights for each neuron layer
         for layer_id in range(len(self.neuron_layers)):
-            self.neuron_layers[layer_id] -= 0.05 * layers_dq_dth[layer_id]
+            self.neuron_layers[layer_id] -= self.learning_rate * layers_dq_dth[layer_id]
 
         return sum(pow(self.layers_output[-1] - desired_output, 2))
-
-
-# # xy = np.asarray([-2, -1, 0, 1, 2])
-# # a = activation_fun(xy, 2)
-# # print(activation_fun_dv(a))
-# nn = NeuralNetwork(2, np.asarray([10, 1]))
-# # inp = np.asarray([1])
-# # outp = np.asarray([6, 2, 4, 8, 10])
-# arr = np.loadtxt("tiny.txt")
-# print(arr)
-# for k in range(1000):
-#     suma = 0
-#     for row in arr:
-#         suma += nn.propagate(row[1:], 1 if row[0] == 1 else -1)
-#     for row in arr:
-#         # print(row[0])
-#         nn.run(row[1:])
-#     print(suma)
-#     if suma < 0.1:
-#         break
-#
-# print(k)
-# print(suma)
-# print(np.ones([5, 5]) * np.asarray([1, 2, 3, 4, 5]))
-# # for k in range(20):
-# #     suma = 0
-# #     for i in range(100):
-# #         for j in range(100):
-# #             inp = np.asarray([i, j])
-# #             outp = np.asarray([4 if j > i else 0, 3 if 2*j > i else 1])
-# #             suma += nn.propagate(inp, outp)
-# #     print(suma)
-# #     print(nn.run(np.asarray([2, 3])))
-#
-#
-# # b = np.asarray([[1, 2, 3], [4, 5, 6]])
-# # c = np.asarray([-1, 2])
-# #
-# # print(b@np.append(c, 1))
--- dane.py
+++ dane.py
@@ -1,6 +1,13 @@
+# PSZT-ML Neural Network project
+# Hanas Marcin, Tuzimek Radosław
+# PW
 import csv
 import numpy as np
 from NeuralNetwork import NeuralNetwork
+
+
+np.random.seed(53645)
+np.set_printoptions(precision=2, floatmode='maxprec_equal')
 
 
 def convert_data(ind, headline, course):
@@ -11,34 +18,23 @@ def convert_data(ind, headline, course):
             if headline[c] == 'school':
                 if row[c] == 'GP':
                     data[r, c] = 1
-                else:
-                    data[r, c] = 0
 
-            if headline[c] == 'sex':
+            elif headline[c] == 'sex':
                 if row[c] == 'F':
                     data[r, c] = 1
-                else:
-                    data[r, c] = 0
-
-            if headline[c] == 'address':
+            elif headline[c] == 'address':
                 if row[c] == 'U':
                     data[r, c] = 1
-                else:
-                    data[r, c] = 0
 
-            if headline[c] == 'famsize':
+            elif headline[c] == 'famsize':
                 if row[c] == 'GT3':
                     data[r, c] = 1
-                else:
-                    data[r, c] = 0
 
-            if headline[c] == 'Pstatus':
+            elif headline[c] == 'Pstatus':
                 if row[c] == 'T':
                     data[r, c] = 1
-                else:
-                    data[r, c] = 0
 
-            if headline[c] == 'Mjob':
+            elif headline[c] == 'Mjob':
                 if row[c] == 'teacher':
                     data[r, c] = 1
                 elif row[c] == 'health':
@@ -51,7 +47,7 @@ def convert_data(ind, headline, course):
                     data[r, c+4] = 1
                 colShift = colShift + 4
 
-            if headline[c] == 'Fjob':
+            elif headline[c] == 'Fjob':
                 if row[c] == 'teacher':
                     data[r, c+colShift] = 1
                 elif row[c] == 'health':
@@ -64,7 +60,7 @@ def convert_data(ind, headline, course):
                     data[r, c+colShift+4] = 1
                 colShift = colShift + 4
 
-            if headline[c] == 'reason':
+            elif headline[c] == 'reason':
                 if row[c] == 'home':
                     data[r, c+colShift] = 1
                 elif row[c] == 'reputation':
@@ -75,7 +71,7 @@ def convert_data(ind, headline, course):
                     data[r, c+colShift+3] = 1
                 colShift = colShift + 3
 
-            if headline[c] == 'guardian':
+            elif headline[c] == 'guardian':
                 if row[c] == 'mother':
                     data[r, c+colShift] = 1
                 elif row[c] == 'father':
@@ -84,65 +80,62 @@ def convert_data(ind, headline, course):
                     data[r, c+colShift+2] = 1
                 colShift = colShift + 2
 
-            if headline[c] == 'schoolsup':
+            elif headline[c] == 'schoolsup':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'famsup':
+            elif headline[c] == 'famsup':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'paid':
+            elif headline[c] == 'paid':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'activities':
+            elif headline[c] == 'activities':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'nursery':
+            elif headline[c] == 'nursery':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'higher':
+            elif headline[c] == 'higher':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'internet':
+            elif headline[c] == 'internet':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'romantic':
+            elif headline[c] == 'romantic':
                 if row[c] == 'yes':
                     data[r, c+colShift] = 1
-                else:
-                    data[r, c+colShift] = 0
 
-            if headline[c] == 'age' or headline[c] == 'Medu' or headline[c] == 'Fedu' or headline[c] == 'traveltime' or \
+            elif headline[c] == 'age' or headline[c] == 'Medu' or headline[c] == 'Fedu' or headline[c] == 'traveltime' or \
                     headline[c] == 'studytime' or headline[c] == 'failures' or headline[c] == 'famrel' or headline[
                 c] == 'freetime' or headline[c] == 'goout' or headline[c] == 'Dalc' or headline[c] == 'Walc' or \
                     headline[c] == 'health' or headline[c] == 'absences' or headline[c] == 'G1' or headline[
                 c] == 'G2' or headline[c] == 'G3':
                 data[r, c+colShift] = row[c]
 
-            if headline[c] == 'course':
-                data[r, c+colShift] = course
-        r = r + 1
+            elif headline[c] == 'mat':
+                if course == 0:
+                    data[r, c+colShift] = 1
 
+            elif headline[c] == 'por':
+                if course == 1:
+                    data[r, c+colShift] = 1
+
+        indices = [np.all(np.equal(data[i, 0:-2], data[r, 0:-2])) and i != r for i in range(data.shape[0])]
+        if sum(indices) > 0:
+            print(data[indices, -1])
+            if course == 1:
+                data[indices, -1][0] = 1
+            else:
+                data[indices, -2][0] = 1
+        else:
+            r = r + 1
     return r
 
 
@@ -150,16 +143,18 @@ with open('student-mat.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     headlineMat = next(csvreader)
     rowNumberMat = len(csvfile.readlines())
-    headlineMat.insert(len(headlineMat), "course")
+    headlineMat.insert(len(headlineMat), "mat")
+    headlineMat.insert(len(headlineMat), "por")
 
 with open('student-por.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     headlinePor = next(csvreader)
     rowNumberPor = len(csvfile.readlines())
-    headlinePor.insert(len(headlinePor), "course")
+    headlinePor.insert(len(headlinePor), "mat")
+    headlinePor.insert(len(headlinePor), "por")
 
-headline = ['school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu', 'Mjob_teacher', 'Mjob_health', 'Mjob_services', 'Mjob_at_home', 'Mjob_other', 'Fjob_teacher', 'Fjob_health', 'Fjob_services', 'Fjob_at_home', 'Fjob_other', 'reason_home', 'reason_reputation', 'reason_course', 'reason_other', 'guardian_mother', 'guardian_father', 'guardian_other', 'traveltime', 'studytime', 'failures', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'Dalc', 'Walc', 'health', 'absences', 'G1', 'G2', 'G3', 'course']
-data = np.empty([rowNumberMat+rowNumberPor, len(headline)])
+headline = ['school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu', 'Mjob_teacher', 'Mjob_health', 'Mjob_services', 'Mjob_at_home', 'Mjob_other', 'Fjob_teacher', 'Fjob_health', 'Fjob_services', 'Fjob_at_home', 'Fjob_other', 'reason_home', 'reason_reputation', 'reason_course', 'reason_other', 'guardian_mother', 'guardian_father', 'guardian_other', 'traveltime', 'studytime', 'failures', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery', 'higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'Dalc', 'Walc', 'health', 'absences', 'G1', 'G2', 'G3', 'mat', 'por']
+data = np.zeros([rowNumberMat+rowNumberPor, len(headline)])
 rowIndex = 0
 
 with open('student-mat.csv', 'r') as csvfile:
@@ -174,37 +169,140 @@ with open('student-por.csv', 'r') as csvfile:
 
     rowIndex = convert_data(rowIndex, headlinePor, 1)
 
-for a in range(rowIndex):
-    print(data[a,:])
+data = data[0:rowIndex, :]
 
-nn_desired_output = -1 * np.ones([data.shape[0], 5])
-for i in range(data.shape[0]):
-    nn_desired_output[i, int(data[i, [x == "Dalc" for x in headline]][0] - 1)] = 1
-    print(nn_desired_output[i, :])
+# Choose learn and test data randomly from data
+print(data.shape)
+rand_indices = np.random.permutation(data.shape[0])
+train_data = data[list(rand_indices[0:3*data.shape[0]//4]), :]
+test_data = data[list(rand_indices[3*data.shape[0]//4:data.shape[0]]), :]
 
-data = data - np.ones(data.shape) * np.average(data, axis=0)
-data = data / (np.ones(data.shape) * np.std(data, axis=0))
-nn_input = data[:, [not x.endswith("alc") for x in headline]]
+# Create histogram data
+alc_count = np.zeros([5, 5, 2])
+for i in range(train_data.shape[0]):
+    alc_count[int(train_data[i, [x == "Dalc" for x in headline]][0] - 1), int(train_data[i, [x == "Walc" for x in headline]][0] - 1), 0] += 1
+for i in range(test_data.shape[0]):
+    alc_count[int(test_data[i, [x == "Dalc" for x in headline]][0] - 1), int(test_data[i, [x == "Walc" for x in headline]][0] - 1), 1] += 1
 
-nn_dalc = NeuralNetwork(nn_input.shape[1], np.asarray([20, 20, 5]))
+print("Liczba probek dla zbioru uczacego:")
+print(alc_count[:, :, 0])
+print(np.sum(alc_count[:, :, 0], axis=0))
+print(np.sum(alc_count[:, :, 0], axis=1))
+print("Liczba probek dla zbioru testujacego:")
+print(alc_count[:, :, 1])
+print(np.sum(alc_count[:, :, 1], axis=0))
+print(np.sum(alc_count[:, :, 1], axis=1))
+print("Liczba próbek dla wszystkich danych:")
+print(np.sum(alc_count, axis=2))
 
-for epoch in range(1000):
+# Create vectors with desired network outputs for test and train datasets
+train_desired_dalc_output = -1 * np.ones([train_data.shape[0], 5])
+train_desired_walc_output = -1 * np.ones([train_data.shape[0], 5])
+test_desired_dalc_output = -1 * np.ones([test_data.shape[0], 5])
+test_desired_walc_output = -1 * np.ones([test_data.shape[0], 5])
+for i in range(train_data.shape[0]):
+    train_desired_dalc_output[i, int(train_data[i, [x == "Dalc" for x in headline]][0] - 1)] = 1
+    train_desired_walc_output[i, int(train_data[i, [x == "Walc" for x in headline]][0] - 1)] = 1
+for i in range(test_data.shape[0]):
+    test_desired_dalc_output[i, int(test_data[i, [x == "Dalc" for x in headline]][0] - 1)] = 1
+    test_desired_walc_output[i, int(test_data[i, [x == "Walc" for x in headline]][0] - 1)] = 1
 
-    for i in np.random.permutation(np.asarray(tuple(range(nn_input.shape[0])))):
-        nn_dalc.propagate(nn_input[i, :], nn_desired_output[i, :])
+train_data = train_data[:, [not x.endswith("alc") for x in headline]]
+test_data = test_data[:, [not x.endswith("alc") for x in headline]]
 
-    errors = np.ndarray([nn_input.shape[0]])
-    results = -1 * np.ones([nn_input.shape[0], nn_desired_output.shape[1]])
-    error_matrix = np.zeros([5, 6])
-    for i in range(nn_input.shape[0]):
-        output = nn_dalc.run(nn_input[i, :])
-        error_matrix[np.argmax(nn_desired_output[i, :]), np.argmax(output)] += 1
-        results[i, np.argmax(output)] = 1
-        errors[i] = not np.array_equal(results[i, :], nn_desired_output[i, :])
+# Calculate means and std
+train_mean = np.mean(train_data, axis=0)
+train_std = np.std(train_data, axis=0)
+print(train_mean)
+print(train_std)
 
-    print(np.average(errors))
-    print(error_matrix)
-    np.savetxt("result_"+str(epoch), results)
-    np.savetxt("error_matrix_"+str(epoch), error_matrix)
-    print(np.average(results, axis=0))
+# Normalize train and test data using mean and std of the train data
+train_data = train_data - np.ones(train_data.shape) * train_mean
+train_data = train_data / (np.ones(train_data.shape) * train_std)
 
+test_data = test_data - np.ones(test_data.shape) * train_mean
+test_data = test_data / (np.ones(test_data.shape) * train_std)
+
+# features_count = 25 # data.shape[1]
+# nn_train_input = train_data[:, np.argsort(train_std)[-features_count:]]
+# nn_test_input = test_data[:, np.argsort(train_std)[-features_count:]]
+nn_train_input = train_data
+nn_test_input = test_data
+# Choose attributes that will be the input for neural network
+# nn_train_input = nn_train_input[:, 0:-20]
+# nn_test_input = nn_test_input[:, 0:-20]
+
+# Create neural network for dalc
+nn_dalc = NeuralNetwork(nn_train_input.shape[1], np.asarray([30, 5]), 0.025)
+
+for epoch in range(50):
+    print(epoch)
+    for i in np.random.permutation(np.asarray(tuple(range(nn_train_input.shape[0])))):
+        nn_dalc.propagate(nn_train_input[i, :], train_desired_dalc_output[i, :])
+
+    # true, if element was classified incorrectly
+    errors_train = np.ndarray([nn_train_input.shape[0]])
+    # transformed output of neural network, for each element 1 for choosen class, -1 for the rest
+    results_train = -1 * np.ones([nn_train_input.shape[0], train_desired_dalc_output.shape[1]])
+    # matrix with count, which class the element really is (row id) and in which was it classified (column id)
+    error_matrix_train = np.zeros([5, 5])
+    for i in range(nn_train_input.shape[0]):
+        output = nn_dalc.run(nn_train_input[i, :])
+        error_matrix_train[np.argmax(train_desired_dalc_output[i, :]), np.argmax(output)] += 1
+        results_train[i, np.argmax(output)] = 1
+        errors_train[i] = not np.array_equal(results_train[i, :], train_desired_dalc_output[i, :])
+
+    # true, if element was classified incorrectly
+    errors_test = np.ndarray([nn_test_input.shape[0]])
+    # transformed output of neural network, for each element 1 for choosen class, -1 for the rest
+    results_test = -1 * np.ones([nn_test_input.shape[0], test_desired_dalc_output.shape[1]])
+    # matrix with count, which class the element really is (row id) and in which was it classified (column id)
+    error_matrix_test = np.zeros([5, 5])
+    for i in range(nn_test_input.shape[0]):
+        output = nn_dalc.run(nn_test_input[i, :])
+        error_matrix_test[np.argmax(test_desired_dalc_output[i, :]), np.argmax(output)] += 1
+        results_test[i, np.argmax(output)] = 1
+        errors_test[i] = not np.array_equal(results_test[i, :], test_desired_dalc_output[i, :])
+
+    print(np.average(errors_train))
+    print(error_matrix_train)
+    print(np.average(errors_test))
+    print(error_matrix_test)
+
+
+# Create neural network for dalc
+nn_walc = NeuralNetwork(nn_train_input.shape[1], np.asarray([60, 5]), 0.028)
+
+for epoch in range(50):
+    print(epoch)
+    for i in np.random.permutation(np.asarray(tuple(range(nn_train_input.shape[0])))):
+        nn_walc.propagate(nn_train_input[i, :], train_desired_walc_output[i, :])
+
+    # true, if element was classified incorrectly
+    errors_train = np.ndarray([nn_train_input.shape[0]])
+    # transformed output of neural network, for each element 1 for choosen class, -1 for the rest
+    results_train = -1 * np.ones([nn_train_input.shape[0], train_desired_walc_output.shape[1]])
+    # matrix with count, which class the element really is (row id) and in which was it classified (column id)
+    error_matrix_train = np.zeros([5, 5])
+    for i in range(nn_train_input.shape[0]):
+        output = nn_walc.run(nn_train_input[i, :])
+        error_matrix_train[np.argmax(train_desired_walc_output[i, :]), np.argmax(output)] += 1
+        results_train[i, np.argmax(output)] = 1
+        errors_train[i] = not np.array_equal(results_train[i, :], train_desired_walc_output[i, :])
+
+    # true, if element was classified incorrectly
+    errors_test = np.ndarray([nn_test_input.shape[0]])
+    # transformed output of neural network, for each element 1 for chosen class, -1 for the rest
+    results_test = -1 * np.ones([nn_test_input.shape[0], test_desired_walc_output.shape[1]])
+    # matrix with count, which class the element really is (row id) and in which was it classified (column id)
+    error_matrix_test = np.zeros([5, 5])
+    for i in range(nn_test_input.shape[0]):
+        output = nn_walc.run(nn_test_input[i, :])
+        error_matrix_test[np.argmax(test_desired_walc_output[i, :]), np.argmax(output)] += 1
+        results_test[i, np.argmax(output)] = 1
+        errors_test[i] = not np.array_equal(results_test[i, :], test_desired_walc_output[i, :])
+
+    print(np.average(errors_train))
+    print(np.average(errors_test))
+    print(error_matrix_train)
+    print(error_matrix_test)
